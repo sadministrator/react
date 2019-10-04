8896a5af14600317e7300242f513be6b47356898
Marco Serrano <marcoTheWebDev@gmail.com>
2019-10-03 23:13:38 -0500

    Implemented routes to HomeComponent and QuotesPageComponent.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import Clock from './components/clockComponent'
 import MyNavbar from './components/myNavbarComponent'
 import QuotesPage from './components/quotesPageComponent'
 import Home from './components/homeComponent'
-
+import { Router } from '@reach/router'
 function App() {
+let home = () => <Clock />
+let quotesPage = () => <MyNavbar />
+
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Home path='/' />
+        <QuotesPage path='the_Donald' />
+      </Router>
     </div>
   );
 }
--- src/components/myNavbarComponent.jsx
+++ src/components/myNavbarComponent.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Clock from './clockComponent'
+import { Router, Link } from '@reach/router'
 
 export default function MyNavbar(props) {
         return (
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">Website.com</Navbar.Brand>
+                <Navbar.Brand href="/">Website.com</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#the_Donald">The Donald</Nav.Link>
+                        <Link to="/">Home</Link>
+                        <pre>  </pre>
+                        <Link to="the_Donald">The Donald</Link>
                     </Nav>
                 </Navbar.Collapse>
                 <Clock />
